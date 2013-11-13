977152b3056abc38f4d9fd4b8c02f8916f7e2d13
Flah <flah.ahmad@haw-hamburg.de>
2013-11-13 16:29:53 +0100

    GUI erstellt und Klassen definiert

---
+++ src/client/Client.java
@@ -0,0 +1,10 @@
+package client;
+
+public class Client {
+    
+    //Verbindung hersellen zum Server, sowie Job ausführen
+    public void startJob() {
+//        ThreadA a = new ThreadA();
+//        ThreadB b = new ThreadB();
+    }
+}
